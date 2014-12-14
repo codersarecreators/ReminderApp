201e412d79633b27f44650ccefbdc097056a5fc4
codersarecreators <bhise.siddharth14890@gmail.com>
2014-12-14 17:25:02 +0530

    committing UI related changes

--- Reminder/gen/com/codersarecreators/reminder/R.java
+++ Reminder/gen/com/codersarecreators/reminder/R.java
@@ -23,31 +23,33 @@ public final class R {
     public static final class drawable {
         public static final int button_delete=0x7f020000;
         public static final int button_image=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int picture=0x7f020003;
-        public static final int picture_minus=0x7f020004;
-        public static final int picture_pressed=0x7f020005;
-        public static final int picture_pressed_minus=0x7f020006;
-        public static final int plus=0x7f020007;
+        public static final int functionalitybtnshape=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int picture=0x7f020004;
+        public static final int picture_minus=0x7f020005;
+        public static final int picture_pressed=0x7f020006;
+        public static final int picture_pressed_minus=0x7f020007;
+        public static final int plus=0x7f020008;
+        public static final int roundshapebtn=0x7f020009;
     }
     public static final class id {
-        public static final int TimeTextBox=0x7f080008;
         public static final int action_settings=0x7f080010;
         public static final int add_Reminder=0x7f080002;
         public static final int backButton=0x7f08000e;
-        public static final int button1=0x7f080006;
         public static final int cancelReminder=0x7f08000b;
         public static final int checkbox=0x7f08000f;
         public static final int chkschedulesms=0x7f080009;
         public static final int contacts=0x7f08000c;
+        public static final int dateSelector=0x7f080005;
         public static final int deleteButton=0x7f08000d;
         public static final int delete_Reminder=0x7f080003;
         public static final int headerText=0x7f080000;
         public static final int listView=0x7f080001;
         public static final int notes=0x7f080004;
         public static final int saveReminder=0x7f08000a;
-        public static final int textView1=0x7f080005;
-        public static final int time=0x7f080007;
+        public static final int textView1=0x7f080006;
+        public static final int textView2=0x7f080008;
+        public static final int timeSelector=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- Reminder/src/com/codersarecreators/reminder/addReminder.java
+++ Reminder/src/com/codersarecreators/reminder/addReminder.java
@@ -2,34 +2,34 @@ package com.codersarecreators.reminder;
 
 import java.util.Calendar;
 import java.util.StringTokenizer;
-
 import android.app.Activity;
-import android.app.AlertDialog.Builder;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.provider.Contacts;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class addReminder extends Activity {
 	
 	 Button btn ;
-	 EditText txtView;
+	 TextView txtView1, txtView2;
 	 String initialDate;
 	 private String initialMonth;
 	 private String initialYear;
-	 private DatePickerDialog dialog = null;
-	 private Calendar calendar;
-	  
-	   private int year, month, day;
+	 private String initialHr;
+	 private String initialMin;
+	 private DatePickerDialog dateDialog = null;
+	 private TimePickerDialog timeDialog = null;
+	 private int year, month, day;
+	 private int hr,min;
+	 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_reminder);
@@ -45,25 +45,22 @@ public class addReminder extends Activity {
     }
 	
 	//Display date picker dialogue
-	@SuppressWarnings("deprecation")
 	public void displayDatePicker(View view){
 		Calendar calendar = null;
-		txtView = (EditText) findViewById(R.id.textView1);
-        String preExistingDate = (String) txtView.getText().toString();
+		txtView1 = (TextView) findViewById(R.id.textView1);
+        String preExistingDate = (String) txtView1.getText().toString();
         
         if(preExistingDate != null && !preExistingDate.equals("")){
             StringTokenizer st = new StringTokenizer(preExistingDate,"/");
                 initialMonth = st.nextToken();
                 initialDate = st.nextToken();
                 initialYear = st.nextToken();
-                if(dialog == null)
-                dialog = new DatePickerDialog(view.getContext(),
+                if(dateDialog == null)
+                dateDialog = new DatePickerDialog(view.getContext(),
                                  new PickDate(),Integer.parseInt(initialYear),
                                  Integer.parseInt(initialMonth),
                                  Integer.parseInt(initialDate));
-               /* dialog.updateDate(Integer.parseInt(initialYear),
-                                 Integer.parseInt(initialMonth),
-                                 Integer.parseInt(initialDate));*/
+               
                 
         } else {
         	calendar = Calendar.getInstance();
@@ -71,14 +68,13 @@ public class addReminder extends Activity {
             month = calendar.get(Calendar.MONTH);
             day = calendar.get(Calendar.DAY_OF_MONTH);
           
-            if(dialog == null)
-            dialog = new DatePickerDialog(view.getContext(),new PickDate(),year,month,
+            if(dateDialog == null)
+            dateDialog = new DatePickerDialog(view.getContext(),new PickDate(),year,month,
                                                day);
-           // dialog.updateDate(dtTxt.getTime().getYear(),dtTxt.getTime().getMonth(),
-           //                                     dtTxt.getTime().getDay());
+           
         }
           
-          dialog.show();
+          dateDialog.show();
       }
       
   	
@@ -109,15 +105,56 @@ public class addReminder extends Activity {
 	}
 	
 	
-	
+	//Date Picker Listener
 	private class PickDate implements DatePickerDialog.OnDateSetListener {
 
 		@Override
 		public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-			// TODO Auto-generated method stub
-		//	view.updateDate(year, monthOfYear, dayOfMonth);
-	        txtView.setText(monthOfYear+1+"/"+dayOfMonth+"/"+year);
-	        dialog.hide();
+		
+	        txtView1.setText(monthOfYear+1+"/"+dayOfMonth+"/"+year);
+	        dateDialog.hide();
 		}
 }
+	
+	// Display Time Picker Dialogue
+	public void displayTimePicker(View view){
+		Calendar calendar = null;
+		txtView2 = (TextView) findViewById(R.id.textView2);
+        String preExistingTime = (String) txtView2.getText().toString();
+        
+        if(preExistingTime != null && !preExistingTime.equals("")){
+            StringTokenizer st = new StringTokenizer(preExistingTime,":");
+                initialHr = st.nextToken();
+                initialMin = st.nextToken();
+                if(timeDialog == null)
+                	timeDialog = new TimePickerDialog(view.getContext(),
+                                 new PickTime(),Integer.parseInt(initialHr),
+                                 Integer.parseInt(initialMin),false);
+              
+                
+        } else {
+        	calendar = Calendar.getInstance();
+            hr = calendar.get(Calendar.HOUR);
+            min = calendar.get(Calendar.MINUTE);
+           
+          
+            if(timeDialog == null)
+            timeDialog = new TimePickerDialog(view.getContext(),new PickTime(),hr,min,
+                                               false);
+          
+        }
+          
+          timeDialog.show();
+      }
+      
+	// Time Picker Listener
+	private class PickTime implements TimePickerDialog.OnTimeSetListener {
+
+		@Override
+		public void onTimeSet(TimePicker view, int hr, int min) {
+			txtView2.setText(hr+":"+min);
+			timeDialog.hide();
+		}
 	}
+	
+}
