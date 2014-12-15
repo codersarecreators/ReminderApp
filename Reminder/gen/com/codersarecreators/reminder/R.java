473b2e317f08e703cf9923bb6bc00fb1521cd254
codersarecreators <bhise.siddharth14890@gmail.com>
2014-12-15 19:08:21 +0530

    Implemented Color theme Worked on Schedule SMS

--- Reminder/gen/com/codersarecreators/reminder/R.java
+++ Reminder/gen/com/codersarecreators/reminder/R.java
@@ -10,6 +10,9 @@ package com.codersarecreators.reminder;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int checkbox_color_add_reminder_screen=0x7f070000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -23,33 +26,42 @@ public final class R {
     public static final class drawable {
         public static final int button_delete=0x7f020000;
         public static final int button_image=0x7f020001;
-        public static final int functionalitybtnshape=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int picture=0x7f020004;
-        public static final int picture_minus=0x7f020005;
-        public static final int picture_pressed=0x7f020006;
-        public static final int picture_pressed_minus=0x7f020007;
-        public static final int plus=0x7f020008;
-        public static final int roundshapebtn=0x7f020009;
+        public static final int checkbox_color_add_reminder_screen=0x7f020002;
+        public static final int contacts=0x7f020003;
+        public static final int functionalitybtnshape=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int picture=0x7f020006;
+        public static final int picture_minus=0x7f020007;
+        public static final int picture_pressed=0x7f020008;
+        public static final int picture_pressed_minus=0x7f020009;
+        public static final int plus=0x7f02000a;
+        public static final int roundshapebtn=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080010;
-        public static final int add_Reminder=0x7f080002;
-        public static final int backButton=0x7f08000e;
-        public static final int cancelReminder=0x7f08000b;
-        public static final int checkbox=0x7f08000f;
-        public static final int chkschedulesms=0x7f080009;
-        public static final int contacts=0x7f08000c;
-        public static final int dateSelector=0x7f080005;
-        public static final int deleteButton=0x7f08000d;
-        public static final int delete_Reminder=0x7f080003;
-        public static final int headerText=0x7f080000;
-        public static final int listView=0x7f080001;
-        public static final int notes=0x7f080004;
-        public static final int saveReminder=0x7f08000a;
-        public static final int textView1=0x7f080006;
-        public static final int textView2=0x7f080008;
-        public static final int timeSelector=0x7f080007;
+        public static final int action_settings=0x7f090017;
+        public static final int add_Reminder=0x7f090002;
+        public static final int backButton=0x7f090015;
+        public static final int cancelReminder=0x7f09000b;
+        public static final int cancelSelectContacts=0x7f090013;
+        public static final int checkbox=0x7f090016;
+        public static final int chkschedulesms=0x7f090009;
+        public static final int contacts=0x7f09000d;
+        public static final int dateSelector=0x7f090005;
+        public static final int dateSelectorInsideSchedulesms=0x7f09000e;
+        public static final int datetextviewInsideSchedulesms=0x7f09000f;
+        public static final int deleteButton=0x7f090014;
+        public static final int delete_Reminder=0x7f090003;
+        public static final int headerText=0x7f090000;
+        public static final int listView=0x7f090001;
+        public static final int notes=0x7f090004;
+        public static final int saveContactDetails=0x7f090012;
+        public static final int saveReminder=0x7f09000a;
+        public static final int smsBody=0x7f09000c;
+        public static final int textView1=0x7f090006;
+        public static final int textView2=0x7f090008;
+        public static final int timeSelector=0x7f090007;
+        public static final int timeSelectorInsideSchedulesms=0x7f090010;
+        public static final int timetextviewInsideSchedulesms=0x7f090011;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -59,7 +71,7 @@ public final class R {
         public static final int single_list=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int Contacts=0x7f05000b;
@@ -67,11 +79,14 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int app_reminder=0x7f050003;
         public static final int cancelReminder=0x7f05000a;
+        public static final int cancelSelectContacts=0x7f05000d;
         public static final int date=0x7f050005;
         public static final int headerText=0x7f050002;
         public static final int notes=0x7f050004;
+        public static final int saveContactDetails=0x7f05000e;
         public static final int setDate=0x7f050006;
         public static final int setReminder=0x7f050009;
+        public static final int smsText=0x7f05000c;
         public static final int text_schedulesms=0x7f050008;
         public static final int time=0x7f050007;
     }
--- Reminder/src/com/codersarecreators/reminder/addReminder.java
+++ Reminder/src/com/codersarecreators/reminder/addReminder.java
@@ -11,6 +11,9 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -19,20 +22,39 @@ import android.widget.Toast;
 public class addReminder extends Activity {
 	
 	 Button btn ;
-	 TextView txtView1, txtView2;
+	 TextView txtView1, txtView2, dateInsideSchedulesms, timeInsideSchedulesms;
 	 String initialDate;
 	 private String initialMonth;
 	 private String initialYear;
 	 private String initialHr;
 	 private String initialMin;
 	 private DatePickerDialog dateDialog = null;
+	 private DatePickerDialog dateDialogInsideScheduleSMS = null;
+	 private TimePickerDialog timeDialogInsideShcheduleSMS = null;
 	 private TimePickerDialog timeDialog = null;
 	 private int year, month, day;
 	 private int hr,min;
+	 CheckBox smsCheckbox;
 	 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_reminder);
+		
+		// Whenever a user clicks on Checkbox , a dialogue should open which prompts for Contact number and if a user unchecks the checkbox, no dialogue should be displayed and the schedule sms should be cancelled.
+		smsCheckbox = (CheckBox)findViewById(R.id.chkschedulesms);
+		smsCheckbox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+					if(isChecked){
+						SMSDialogue(buttonView);
+					}
+					else{
+						MyToast.RaiseToast("Schedule SMS has been removed from list");
+					}
+				
+			}
+		});
 		
 		
 	}
@@ -87,19 +109,36 @@ public class addReminder extends Activity {
 	public void SMSDialogue(View view){
 		final Dialog dialog = new Dialog(view.getContext());
 		dialog.setCancelable(true);
-		dialog.setTitle("Mobile Number");
+		dialog.setTitle("Schedule SMS");
 		dialog.setContentView(R.layout.contacts_dialog);
 		Button selectContacts =(Button)dialog.findViewById(R.id.contacts);
 		selectContacts.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(v.getContext(), "Inside selectContacts",Toast.LENGTH_LONG ).show();
+				Toast.makeText(v.getContext(),"Clicked on Contacts icon",Toast.LENGTH_LONG ).show();
 
 			}
 			
 		});
-		
+		Button dateBtn = (Button)dialog.findViewById(R.id.dateSelectorInsideSchedulesms);
+		dateBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				displayDatePickerInsideSchedulesms(dialog);
+			}
+		});
+		Button timeBtn = (Button)dialog.findViewById(R.id.timeSelectorInsideSchedulesms);
+		timeBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				displayTimePickerInsideSchedulesms(dialog);
+			}
+		});
 		  
 		dialog.show();
 	}
@@ -157,4 +196,95 @@ public class addReminder extends Activity {
 		}
 	}
 	
+	/**
+	 * This function display Date picker dialog inside Select Contacts dialog
+	 * @param dialog
+	 */
+	public void displayDatePickerInsideSchedulesms(Dialog dialog){
+		Calendar calendar = null;
+		dateInsideSchedulesms = (TextView)dialog.findViewById(R.id.datetextviewInsideSchedulesms);
+        String preExistingDate = (String) dateInsideSchedulesms.getText().toString();
+        
+        if(preExistingDate != null && !preExistingDate.equals("")){
+            StringTokenizer st = new StringTokenizer(preExistingDate,"/");
+                initialMonth = st.nextToken();
+                initialDate = st.nextToken();
+                initialYear = st.nextToken();
+                if(dateDialogInsideScheduleSMS == null)
+                dateDialogInsideScheduleSMS = new DatePickerDialog(dialog.getContext(),
+                                 new PickDateInsideScheduleSMS(),Integer.parseInt(initialYear),
+                                 Integer.parseInt(initialMonth),
+                                 Integer.parseInt(initialDate));
+               
+                
+        } else {
+        	calendar = Calendar.getInstance();
+            year = calendar.get(Calendar.YEAR);
+            month = calendar.get(Calendar.MONTH);
+            day = calendar.get(Calendar.DAY_OF_MONTH);
+          
+            if(dateDialogInsideScheduleSMS == null)
+            	dateDialogInsideScheduleSMS = new DatePickerDialog(dialog.getContext(),new PickDateInsideScheduleSMS(),year,month,
+                                               day);
+           
+        }
+          
+        dateDialogInsideScheduleSMS.show();
+      }
+      
+	//Date Picker Listener for Date picker Inside Schedule SMS
+		private class PickDateInsideScheduleSMS implements DatePickerDialog.OnDateSetListener {
+
+			@Override
+			public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+			
+				dateInsideSchedulesms.setText(monthOfYear+1+"/"+dayOfMonth+"/"+year);
+		        dateDialogInsideScheduleSMS.hide();
+			}
+	}
+	
+		/**
+		 * This function display Date picker dialog inside Select Contacts dialog
+		 * @param dialog
+		 */
+		public void displayTimePickerInsideSchedulesms(Dialog dialog){
+			Calendar calendar = null;
+			timeInsideSchedulesms = (TextView) dialog.findViewById(R.id.timetextviewInsideSchedulesms);
+	        String preExistingTime = (String) timeInsideSchedulesms.getText().toString();
+	        
+	        if(preExistingTime != null && !preExistingTime.equals("")){
+	            StringTokenizer st = new StringTokenizer(preExistingTime,":");
+	                initialHr = st.nextToken();
+	                initialMin = st.nextToken();
+	                if(timeDialogInsideShcheduleSMS == null)
+	                	timeDialogInsideShcheduleSMS = new TimePickerDialog(dialog.getContext(),
+	                                 new PickTimeInsideScheduleSMS(),Integer.parseInt(initialHr),
+	                                 Integer.parseInt(initialMin),false);
+	              
+	                
+	        } else {
+	        	calendar = Calendar.getInstance();
+	            hr = calendar.get(Calendar.HOUR);
+	            min = calendar.get(Calendar.MINUTE);
+	           
+	          
+	            if(timeDialogInsideShcheduleSMS == null)
+	            	timeDialogInsideShcheduleSMS = new TimePickerDialog(dialog.getContext(),new PickTimeInsideScheduleSMS(),hr,min,
+	                                               false);
+	          
+	        }
+	          
+	          timeDialogInsideShcheduleSMS.show();
+	      }
+	      
+		//Date Picker Listener for Date picker Inside Schedule SMS
+			private class PickTimeInsideScheduleSMS implements TimePickerDialog.OnTimeSetListener {
+
+				@Override
+				public void onTimeSet(TimePicker view, int hr, int min) {
+					timeInsideSchedulesms.setText(hr+" : "+min );
+					timeDialogInsideShcheduleSMS.hide();
+				}
+			}
+		
 }
